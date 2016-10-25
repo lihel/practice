0fca2060b8f04041cf10857b2db88f59cead4ccb
Linux <835943102@qq.com>
2016-10-25 16:07:30 +0800

    add practice2and3

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,3 +1,12 @@
-function count_same_elements(collection) {
-  //在这里写入代码
+function collect_same_elements(collection_a, collection_b) {
+  var k=0,i=0,j=0;
+  var d=[];
+  for(i=0;i<collection_a.length;i++){
+    for(j=0;j<collection_b[0].length;j++)
+    {
+      if (collection_a[i] === collection_b[0][j])
+        d[k++] = collection_a[i];
+    }
+  }
+  return d;
 }
--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,6 +1,14 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
-
+var c=[];
+  var k=0,i=0,j=0;
+  for(i=0;i<collection_a.length;i++){
+    for(j=0;j<object_b.value.length;j++){
+      if(collection_a[i]==object_b.value[j])
+        c[k++]=collection_a[i];
+    }
+  }
+  return c;
 }
 
 
