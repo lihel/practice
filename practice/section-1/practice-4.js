42f6d8fe65bab13eeaa6f727d7535552fc3e8668
Linux <835943102@qq.com>
2016-10-25 22:06:52 +0800

    add practice

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,15 @@
-function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
-}
+function collect_same_elements(collection_a, object_b) {
+  var k = 0;
+  var j = 0;
+  var newArray = [];
+  for (var i = 0; i < collection_a.length; i++) {
 
+    for (j = 0; j < object_b.value.length; j++) {
+
+      if (collection_a[i].key === object_b.value[j])
+        newArray.push(collection_a[i].key);
+    }
+  }
+  return newArray;
+}
 
