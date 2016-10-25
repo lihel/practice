962a38722ee72e1aa4b8d9f8cea9c479e3e3280b
Linux <835943102@qq.com>
2016-10-25 22:20:17 +0800

    add practice

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -3,9 +3,7 @@ function collect_same_elements(collection_a, object_b) {
   var j = 0;
   var newArray = [];
   for (var i = 0; i < collection_a.length; i++) {
-
     for (j = 0; j < object_b.value.length; j++) {
-
       if (collection_a[i].key === object_b.value[j])
         newArray.push(collection_a[i].key);
     }
