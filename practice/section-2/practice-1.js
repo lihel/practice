86df26c3321c00454e3caf60a50db38f72521c2a
Linux <835943102@qq.com>
2016-10-27 18:04:30 +0800

    practice

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,3 +1,25 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var newarr=[];
+  var k=0;
+  for(var j=1;j<9;j++)
+  newarr[j]={};
+  var object = new Object();
+  newarr[0]=object;
+  newarr[0].key=collection[0];
+  newarr[0].count=1;
+  for(var i=1;i<collection.length;i++)
+  {
+     if(collection[i] === newarr[k].key)
+    {
+      newarr[k].count++;
+    }
+    else
+    {
+      k++;
+      newarr[k].key=collection[i];
+      newarr[k].count = 1;
+    }
+  }
+  return newarr;
 }
