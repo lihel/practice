ecf1a24da69d9dd52cc9a8b56d72d4abc4771272
Linux <835943102@qq.com>
2016-10-29 20:31:59 +0800

    add practice

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,10 +1,8 @@
 function count_same_elements(collection) {
   var newarr=[],split_arr=[];
   var k=0;
-  for(var j=1;j<9;j++)
+  for(var j=0;j<9;j++)
     newarr[j]={};
-  var object = new Object();
-  newarr[0]=object;
   newarr[0].key=collection[0];
   newarr[0].count=1;
   for(var i=1;i<collection.length;i++)
--- practice/section-3/practice-3.js
+++ practice/section-3/practice-3.js
@@ -1,3 +1,32 @@
 function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+  var newarr=[];
+  var k=0;
+  for(var j=0;j<9;j++)
+    newarr[j]={};
+  newarr[0].key=collection_a[0];
+  newarr[0].count=1;
+  for(var i=1;i<collection_a.length;i++)
+  {
+    if(collection_a[i] === newarr[k].key)
+    {
+      newarr[k].count++;
+    }
+    else
+    {
+      k++;
+      newarr[k].key=collection_a[i];
+      newarr[k].count = 1;
+    }
+  }
+
+  for(var i=0;i<newarr.length;i++)
+  {
+    for(var j=0;j<object_b.value.length;j++)
+    {
+      if (newarr[i].key === object_b.value[j]) {
+        newarr[i].count -= Math.floor(newarr[i].count/3);
+      }
+    }
+  }
+  return newarr;
 }
